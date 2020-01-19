2dede1112934ffbd57dbc6b2a9f34befa5d72914
brandonperfetti <brandon@brandonperfetti.com>
2020-01-18 17:55:18 -0800

    Generate Dynamic Routes in Gatsby with Reach Router.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onCreatePage = ({ page, actions }) => {
+  if (page.path.match(/^\/room/)) {
+    page.matchPath = "/room/*"
+    actions.createPage(page)
+  }
+}
