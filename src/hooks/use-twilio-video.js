1ebd4d2b511b2c108f7386bf6c62b24f4b67aebf
brandonperfetti <brandon@brandonperfetti.com>
2020-01-18 22:44:47 -0800

    Connect Video to a Room with React Hooks in Gatsby.

--- src/hooks/use-twilio-video.js
+++ src/hooks/use-twilio-video.js
@@ -1,6 +1,6 @@
-import React, { createContext, useContext, useReducer } from "react"
-
+import React, { createContext, useContext, useReducer, useRef } from "react"
 import axios from "axios"
+import { connect } from "twilio-video"
 
 const TWILIO_TOKEN_URL = "https://bisque-peacock-5978.twil.io/create-room-token"
 
@@ -8,6 +8,7 @@ const DEFAULT_STATE = {
   identity: false,
   roomName: false,
   token: false,
+  room: false,
 }
 
 const reducer = (state, action) => {
@@ -20,6 +21,8 @@ const reducer = (state, action) => {
         roomName: action.roomName,
       }
 
+    case "set-active-room":
+      return { ...state, room: action.room }
     default:
       return DEFAULT_STATE
   }
@@ -39,6 +42,7 @@ export const wrapRootElement = ({ element }) => (
 
 const useTwilioVideo = () => {
   const [state, dispatch] = useContext(TwilioVideoContext)
+  const videoRef = useRef()
 
   const getRoomToken = async ({ identity, roomName }) => {
     const result = await axios.post(TWILIO_TOKEN_URL, {
@@ -49,7 +53,34 @@ const useTwilioVideo = () => {
     dispatch({ type: "join", token: result.data, identity, roomName })
   }
 
-  return { state, getRoomToken }
+  const connectToRoom = async () => {
+    if (!state.token) {
+      return
+    }
+
+    const room = await connect(state.token, {
+      name: state.roomName,
+      audio: true,
+      video: { width: 640 },
+      logLevel: "info",
+    }).catch(error => {
+      console.error(`Unable to join the room: ${error.message}`)
+    })
+
+    const localTrack = [...room.localParticipant.videoTracks.values()][0].track
+
+    if (!videoRef.current.hasChildNodes()) {
+      const localEl = localTrack.attach()
+
+      videoRef.current.appendChild(localEl)
+    }
+
+    dispatch({ type: "set-active-room", room })
+  }
+
+  const startVideo = () => connectToRoom()
+
+  return { state, getRoomToken, startVideo, videoRef }
 }
 
 export default useTwilioVideo
