37855d11a688f46e588cbc4cba3f3ff78fea6db7
brandonperfetti <brandon@brandonperfetti.com>
2020-01-18 19:30:02 -0800

    Manage Application State in Gatsby with React Hooks.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,10 @@
 /**
+import { wrapRootElement } from './src/components/hooks/use-twilio-video';
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
 // You can delete this file if you're not using it
+
+export { wrapRootElement } from "./src/hooks/use-twilio-video"
--- src/components/join.js
+++ src/components/join.js
@@ -1,31 +1,41 @@
 import React, { useState } from "react"
+import useTwilioVideo from "../hooks/use-twilio-video"
 
 const Join = () => {
+  const { state, dispatch } = useTwilioVideo()
   const [identity, setIdentity] = useState("")
   const [roomName, setRoomName] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    dispatch({ type: "join", identity, roomName })
+  }
 
   return (
     <>
       <h1>Start or Join a Video Call</h1>
-      <form className="start-form">
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <form className="start-form" onSubmit={handleSubmit}>
         <label htmlfor="identity">
           Display name:
           <input
             type="text"
             id="identity"
             value={identity}
-            onChange={e => setIdentity(e.target.value)}
+            onChange={event => setIdentity(event.target.value)}
           />
         </label>
         <label htmlfor="roomName">
-          Display name:
+          Which room do you want to join?
           <input
             type="text"
             id="roomName"
             value={roomName}
-            onChange={e => setRoomName(e.target.value)}
+            onChange={event => setRoomName(event.target.value)}
           />
         </label>
+        <button type="submit">Join Video Chat</button>
       </form>
     </>
   )
